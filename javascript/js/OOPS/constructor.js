9b91f07157fc9ecfabf050f4c13b1615c533b121
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2023-04-13 19:23:17 +0530

    fix

--- javascript/js/OOPS/constructor.js
+++ javascript/js/OOPS/constructor.js
@@ -1,11 +1,10 @@
 class student {
-  // constructor function 
-  constructor (name , age) {
-    console.log('constructor methord running')
-    this.name = name , 
-    this.age = age 
+  // constructor function
+  constructor(name, age) {
+    console.log("constructor methord running");
+    (this.name = name), (this.age = age);
   }
   //   prototype methord
 }
-let ram = new student("ram" , 26)
-console.log(ram)
+let ram = new student("ram", 26);
+console.log(ram);
