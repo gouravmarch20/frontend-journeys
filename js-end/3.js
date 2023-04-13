3c6cac9aec45b016c2221a42d206a78fe220249c
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2023-04-13 19:23:52 +0530

    fix

--- js-end/3.js
+++
@@ -1,5 +0,0 @@
-//primive : string , number , boolean ,undefinded
-
-//? how string , number have methord ==> wrap  object sai : String , Number  , Boolean
-
-// js privimate does not have methord , property ==> js convert to it's wrap class as he see ==> and execute -> after that garbage collected  object -> they are primitivewith props/methord
--- js-end/this/this-draw.js
+++
@@ -1,5 +0,0 @@
-const Vechical = function (color) {
-  this.color = color
-}
-const redVehical = new Vechical('red')
-console.log(redVehical);
